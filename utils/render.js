3c86300f68ec2c6bfe0e64e8936e118862e7150c
TekF <teknof@gmail.com>
2016-11-23 00:13:20 +0000

    .

---
+++ utils/render.js
@@ -0,0 +1,9 @@
+/*
+initialise resolution from canvas's size,
+=> maybe stick everything into custom tokens on the canvas
+= neat, and no js in index.html
+
+https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Getting_started_with_WebGL
+https://sites.google.com/site/progyumming/javascript/shortest-webgl
+*/
+
