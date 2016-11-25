fc2fe7b5f85496eb71e3695afafe1189d817b644
TekF <teknof@gmail.com>
2016-11-25 10:43:19 +0000

    Update render.js

--- utils/render.js
+++ utils/render.js
@@ -5,5 +5,8 @@ initialise resolution from canvas's size,
 
 https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Getting_started_with_WebGL
 https://sites.google.com/site/progyumming/javascript/shortest-webgl
+
+right way to queue next frame, call window.requestAnimationFrame(draw) at the end of draw.
+https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame - it's a proper implementation of an old less proper way that I used before!
 */
 
